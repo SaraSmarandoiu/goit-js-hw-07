61108e453326d2d02463426d6c166112b89aebe5
SaraSmarandoiu <smarandoiusara@gmail.com>
2024-04-13 14:57:15 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const image = document.createElement("img");
     image.classList.add("gallery__image");
     image.src = item.preview;
-    image.dataset.source = item.original; // Folosim dataset.source pentru a seta atributul data-source
+    image.dataset.source = item.original;
     image.alt = item.description;
 
     link.appendChild(image);
