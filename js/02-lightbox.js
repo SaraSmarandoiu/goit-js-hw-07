a0a84f03a4811ae734a110781dcd04e5d5c2e1f1
SaraSmarandoiu <smarandoiusara@gmail.com>
2024-04-13 16:10:09 +0300

    commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+document.addEventListener('DOMContentLoaded', function () {
+    const gallery = document.querySelector('.gallery');
+  
+    function createGalleryItem(item) {
+      const li = document.createElement('li');
+      li.classList.add('gallery__item');
+  
+      const link = document.createElement('a');
+      link.classList.add('gallery__link');
+      link.href = item.original;
+  
+      const image = document.createElement('img');
+      image.classList.add('gallery__image');
+      image.src = item.preview;
+      image.alt = item.description;
+  
+      link.appendChild(image);
+      li.appendChild(link);
+  
+      return li;
+    }
+  
+    function initializeLightbox() {
+      new SimpleLightbox('.gallery a', {
+        captionsData: 'alt', 
+        captionDelay: 250 
+      });
+    }
+  
+    galleryItems.forEach(item => {
+      const galleryItem = createGalleryItem(item);
+      gallery.appendChild(galleryItem);
+    });
+  
+    initializeLightbox();
+  });
 
 console.log(galleryItems);
