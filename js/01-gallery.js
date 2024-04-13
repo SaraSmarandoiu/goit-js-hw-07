dd873bee24c40c290d3dfd62a0b6f80f00ce7e37
SaraSmarandoiu <smarandoiusara@gmail.com>
2024-04-13 14:56:50 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = document.querySelector(".gallery");
+
+  function createGalleryItem(item) {
+    const li = document.createElement("li");
+    li.classList.add("gallery__item");
+
+    const link = document.createElement("a");
+    link.classList.add("gallery__link");
+    link.href = item.original;
+
+    const image = document.createElement("img");
+    image.classList.add("gallery__image");
+    image.src = item.preview;
+    image.dataset.source = item.original; // Folosim dataset.source pentru a seta atributul data-source
+    image.alt = item.description;
+
+    link.appendChild(image);
+    li.appendChild(link);
+
+    return li;
+  }
+
+  function openModal(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+      return;
+    }
+    const imageURL = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+        <div class="modal">
+          <img src="${imageURL}" width="800" height="600">
+        </div>
+      `);
+    instance.show();
+
+    document.addEventListener("keydown", closeModalOnEscape);
+  }
+
+  // function closeModalOnEscape(event) {
+  // if (event.key === "Escape" && lightboxInstance) {
+  //   lightboxInstance.close();
+  //   document.removeEventListener("keydown", closeModalOnEscape);
+  // }
+  //}
+
+  galleryItems.forEach((item) => {
+    const galleryItem = createGalleryItem(item);
+    gallery.appendChild(galleryItem);
+  });
+
+  gallery.addEventListener("click", openModal);
+});
 console.log(galleryItems);
